aabe5b674767ac91c8f3873bd45d6f58e4f9bf61
Linus Unnebäck <linus@folkdatorn.se>
2016-10-13 15:50:21 +0200

    Print unhandled rejections

--- src/util.js
+++ src/util.js
@@ -65,4 +65,5 @@ export const toLowerCase = s => lcCache[s] || (lcCache[s] = s.toLowerCase());
  *	@param {Function} callback
  */
 let resolved = typeof Promise!=='undefined' && Promise.resolve();
-export const defer = resolved ? (f => { resolved.then(f); }) : setTimeout;
+let errorHandler = err => { console.error(err.toString()); console.error(err.stack); };
+export const defer = resolved ? (f => { resolved.then(f).catch(errorHandler); }) : setTimeout;
